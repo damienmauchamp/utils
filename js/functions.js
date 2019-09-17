6f13ef4042aeb38ad504b513fd800bd411c8b481
Damien Mauchamp <damienmauchamp@gmail.com>
2019-09-17 09:14:12 +0200

    fx

--- js/functions.js
+++ js/functions.js
@@ -35,3 +35,51 @@ $.fn.myPlugin = function() {
 function array_column(arr, column) {
 	return arr.map(x => x[column]);
 }
+
+jQuery.fn.closestToOffset = function(offset) {
+    var el = null,
+        elOffset,
+        x = offset.left,
+        y = offset.top,
+        distance,
+        dx,
+        dy,
+        minDistance;
+    this.each(function() {
+        var $t = $(this);
+        elOffset = $t.offset();
+        right = elOffset.left + $t.width();
+        bottom = elOffset.top + $t.height();
+
+        if (
+            x >= elOffset.left &&
+            x <= right &&
+            y >= elOffset.top &&
+            y <= bottom
+        ) {
+            el = $t;
+            return false;
+        }
+
+        var offsets = [
+            [elOffset.left, elOffset.top],
+            [right, elOffset.top],
+            [elOffset.left, bottom],
+            [right, bottom],
+        ];
+        for (var off in offsets) {
+            dx = offsets[off][0] - x;
+            dy = offsets[off][1] - y;
+            distance = Math.sqrt(dx * dx + dy * dy);
+            if (minDistance === undefined || distance < minDistance) {
+                minDistance = distance;
+                el = $t;
+            }
+        }
+    });
+    return el;
+};
+
+jQuery.fn.deplacer_element_vers = function(cible) {
+    return $(this).detach().appendTo($(cible));
+}
--- php/functions.php
+++ php/functions.php
@@ -1,16 +1,12 @@
 <?php
 
-function ok() {
-	return "ok";
+function in_array_count($needle, $haystack) {
+	return count(array_keys($haystack, $needle));
 }
 
-function test() {
-	$test = "}";
-	$test .= '"';
-	$test .= '}}';
-	$test .= "'" . "'";
-	$test .= '}}';
-	return test;
+function in_array_count_check($needle, $haystack, $times) {
+	$values = array_count_values($haystack);
+	return isset($values[$needle] ? $values[$needle] : 0);
 }
 
 function recuperer_valeur_par_colonne($array, $valeur, $colonne, $retourner_index = false) {
@@ -38,4 +34,51 @@ function ends_with($haystack, $needle)
 {
     $length = strlen($needle);
     return $length === 0 || (substr($haystack, -$length) === $needle);
+}
+
+/**
+ * Vérifie l'existence d'une clé dans un tableau et renvoie sa valeur en conséquence
+ * @param mixed $cle
+ * @param array $tableau
+ * @param mixed $defaut valeur à retourner si la clé n'existe pas
+ * @return mixed
+ */
+function recuperer_valeur_tableau($cle, $tableau, $defaut = false) {
+	if(gettype($tableau) !== 'array' || !$tableau || !array_key_exists($cle, $tableau)) {
+		return $defaut;
+	}
+	return $tableau[$cle];
+}
+
+function remove_accents($str, $charset='utf-8') {
+    $str = htmlentities( $str, ENT_NOQUOTES, $charset );
+    $str = preg_replace( '#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str );
+    $str = preg_replace( '#&([A-za-z]{2})(?:lig);#', '\1', $str );
+    $str = preg_replace( '#&[^;]+;#', '', $str );
+    return $str;
+}
+
+function remove_special_chars($str, $replace = '') {
+	return trim(preg_replace('/[^A-Za-zÀ-ÖØ-öø-ÿ]+/', $replace, $str), $replace);
+}
+
+
+function strto($str, $params = [], $charset = "utf-8") {
+	if (!$params || !is_array($params)) {
+		return $str;		
+	}
+	
+	if (in_array('remove_accents', $params)) {
+		$str = remove_accents($str, $charset);
+	}
+	
+	if (in_array('lowercase', $params)) {
+		$str = mb_strtolower($str);
+	}
+	
+	if (in_array('uppercase', $params)) {
+		$str = mb_strtoupper($str);
+	}
+	
+	return $str;
 }
